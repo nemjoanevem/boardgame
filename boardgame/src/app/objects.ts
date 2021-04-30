384a7917da226d95e34e55a12888717ac44d7ff4
nemjoanevem <58182569+nemjoanevem@users.noreply.github.com>
2021-04-30 15:53:42 +0200

    create event class

--- boardgame/src/app/objects.ts
+++ boardgame/src/app/objects.ts
@@ -3,4 +3,16 @@ export class user{
     name: string;
     email: string;
     password: string;
+    city: string;
+    gender: string;
+}
+
+export class event{
+    id: number;
+    organizer: string;
+    place: string;
+    langauge: string;
+    max_players: number;
+    min_players: number;
+    time: string;
 }
