9de16ff920024213e31c25796e9b353ee3ac655c
nemjoanevem <58182569+nemjoanevem@users.noreply.github.com>
2021-04-30 12:24:42 +0200

    updating login and register

--- boardgame/src/app/login/login.component.ts
+++ boardgame/src/app/login/login.component.ts
@@ -1,3 +1,4 @@
+import { user } from './../objects';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,12 +8,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  loginUser : user = new user;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  register(){
-    
+  login(){
+
+    // localStorage.setItem("validLogin", "false"); Ha ez false akkor nem enged be az oldalra
+    //                                              Ha true akkor beenged
+
+    let login = (<HTMLInputElement>document.getElementById('login')).value;
+    let pwFromLogin = (<HTMLInputElement>document.getElementById('password')).value;
+    if(login.length > 3 && pwFromLogin.length > 5 /* emailValid(login) -> Ha emailes a login */){
+      /* if(van match DB-ben a login-ra){
+
+        itt ki kell kérni a db-ből a loginhoz tartozó jelszót, utána bedobni a függvénybe
+        pl:
+        pwFromDB = password.from.db;
+
+        if(this.asciiToString(pwFromDB) === pwFromLogin){
+          Ekkor enged csak be az oldalra
+        }
+        else{
+          alert("Incorrect password!");
+          localStorage.setItem("validLogin", "false");
+        };
+
+      }
+      else{
+        alert("Incorrect name");
+        localStorage.setItem("validLogin", "false");
+          }; */
+      
+      localStorage.setItem("validLogin", "true");
+      console.log("nice");
+    }
+    else{
+      alert("Incorrect login!");
+      localStorage.setItem("validLogin", "false");
+        };
+  }
+
+  //Ez akkor kell, ha a login email-es
+  /*emailValid = (email : any) => {
+    const emailRegex = /^([A-Za-z0-9_\-.+])+@([A-Za-z0-9_\-.])+\.([A-Za-z]{2,})$/;
+    return emailRegex.test(email);
+  }*/
+
+  asciiToString(asciipw: any){
+    const char = asciipw.split('a');
+    for(const i in char){
+      char[i] = String.fromCharCode(char[i]);
+    }
+    var string = char.join("");
+    return string;
   }
 }
--- boardgame/src/app/register/register.component.ts
+++ boardgame/src/app/register/register.component.ts
@@ -1,15 +1,60 @@
+import { HttpClientModule } from '@angular/common/http';
+import { user } from './../objects';
 import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./../login/login.component.css']
 })
 export class RegisterComponent implements OnInit {
+
+  newUser : user = new user;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  register(){
+    //Ez a "register" oldalon lévő "registerButton" id-s gomb megnyomásakor történik.
+    //Egy "User" object-et tölt fel a regisztráláskor megadott adatokkal
+    //Ezt a user objectet lehet majd küldeni a DB-be
+    //id-t meg a db-ben kap auto incrementel(gondolom)
+    //A jelszót ascii kódként kapja meg a db (nem akartam bonyibbal szórakozni)
+    this.newUser.name = (<HTMLInputElement>document.getElementById('name')).value;
+    this.newUser.email = (<HTMLInputElement>document.getElementById('email')).value;
+    let pw = (<HTMLInputElement>document.getElementById('password')).value;
+    if(this.newUser.name.length > 3){
+      if(this.emailValid(this.newUser.email)){
+        /* if(a név és email cim nincs benne a db-ben {*/
+          if(pw.length > 5){
+            alert("Registration succeced!");
+            this.newUser.password = this.passwordToAscii(pw);
+            /* this.newUser -> DB */
+          }
+          else alert("Password must contain at least 5 characters!");
+
+        /*}
+          else alert("Username or Email is already taken!");*/
+      }
+      else alert("Email is invalid!");
+    }
+    else alert("Name must contain at least 3 characters!");
+  }
+
+  passwordToAscii(pw: any){
+    const char = pw.split('');
+    for(const i in char){
+      char[i] = char[i].charCodeAt(0)+"a";
+    }
+    var string = char.join("");
+    return string;
+  }
+  emailValid = (email : any) => {
+    const emailRegex = /^([A-Za-z0-9_\-.+])+@([A-Za-z0-9_\-.])+\.([A-Za-z]{2,})$/;
+    return emailRegex.test(email);
+}
 }
