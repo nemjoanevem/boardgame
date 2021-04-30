80083aee6ec64581a81fe2323e2f6a101ff03fcb
nemjoanevem <58182569+nemjoanevem@users.noreply.github.com>
2021-04-30 12:25:44 +0200

    update path and guard

--- boardgame/src/app/app.module.ts
+++ boardgame/src/app/app.module.ts
@@ -1,31 +1,37 @@
+import { AuthGuard } from './auth.guard';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import {HttpClientModule} from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     RouterModule.forRoot([
       {path: "login", component: LoginComponent},
       {path: "register", component: RegisterComponent},
+      {path: "main", component: MainComponent, canActivate: [AuthGuard]},
       {
         path: '', redirectTo: '/login',
         pathMatch: 'full'
       }
     ])
   ],
-  providers: [],
+  providers: [AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
